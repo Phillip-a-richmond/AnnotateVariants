e331ade754cda54f54130c8c18b6e5d36f6fe3e7
Phillip-a-richmond <phillip.a.richmond@gmail.com>
2018-01-23 16:27:35 -0800

    Fixed to the newest gnomAD

--- GetGNOMAD.sh
+++ GetGNOMAD.sh
@@ -22,56 +22,56 @@ cd /home/richmonp/scratch/DATABASES/
 
 
 # Get the data from online
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.1.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.2.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.3.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.4.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.5.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.6.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.7.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.8.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.9.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.10.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.11.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.12.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.13.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.14.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.15.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.16.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.17.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.18.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.19.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.20.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.21.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.22.vcf.gz
-nohup wget https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.X.vcf.gz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr1.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr2.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr3.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr4.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr5.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr6.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr7.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr8.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr9.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr10.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr11.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr12.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr13.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr14.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr15.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr16.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr17.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr18.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr19.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr20.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr21.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr22.vcf.bgz
+nohup wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chrX.vcf.bgz
 
 # 1) normalize & decompose VCFs with vt
-
+exit
 REF='/mnt/data/GENOMES/GSC/GRCh37-lite.fa'
 for i in {1..22}
 do 
-	VCFGZ='gnomad.genomes.r2.0.1.sites.'$i'.vcf.gz'
-	NORMVCF='gnomad.genomes.r2.0.1.sites.'$i'.norm.vcf.gz'
+	VCFGZ='gnomad.genomes.r2.0.2.sites.chr'$i'.vcf.bgz'
+	NORMVCF='gnomad.genomes.r2.0.2.sites.chr'$i'.norm.vcf.bgz'
 	
 	zless $VCFGZ  \
 	   | sed 's/ID=AD,Number=./ID=AD,Number=R/' \
 	   | vt decompose -s - \
 	   | vt normalize -r $REF - \
-	   | bgzip -c > $NORMVCF 
+	   | .bgzip -c > $NORMVCF 
 	tabix -p vcf $NORMVCF
 done
 
-VCFGZ='gnomad.genomes.r2.0.1.sites.X.vcf.gz'
-NORMVCF='gnomad.genomes.r2.0.1.sites.X.norm.vcf.gz'
+VCFGZ='gnomad.genomes.r2.0.2.sites.chrX.vcf.bgz'
+NORMVCF='gnomad.genomes.r2.0.2.sites.chrX.norm.vcf.bgz'
 
 zless $VCFGZ  \
            | sed 's/ID=AD,Number=./ID=AD,Number=R/' \
            | vt decompose -s - \
            | vt normalize -r $REF - \
-           | bgzip -c > $NORMVCF
+           | .bgzip -c > $NORMVCF
 tabix -p vcf $NORMVCF
 
 # 2) Concatenate Normalized files
-vcf-concat gnomad.genomes.r2.0.1.sites.1.norm.vcf.gz gnomad.genomes.r2.0.1.sites.2.norm.vcf.gz gnomad.genomes.r2.0.1.sites.3.norm.vcf.gz gnomad.genomes.r2.0.1.sites.4.norm.vcf.gz gnomad.genomes.r2.0.1.sites.5.norm.vcf.gz gnomad.genomes.r2.0.1.sites.6.norm.vcf.gz gnomad.genomes.r2.0.1.sites.7.norm.vcf.gz gnomad.genomes.r2.0.1.sites.8.norm.vcf.gz gnomad.genomes.r2.0.1.sites.9.norm.vcf.gz gnomad.genomes.r2.0.1.sites.10.norm.vcf.gz gnomad.genomes.r2.0.1.sites.11.norm.vcf.gz gnomad.genomes.r2.0.1.sites.12.norm.vcf.gz gnomad.genomes.r2.0.1.sites.13.norm.vcf.gz gnomad.genomes.r2.0.1.sites.14.norm.vcf.gz gnomad.genomes.r2.0.1.sites.15.norm.vcf.gz gnomad.genomes.r2.0.1.sites.16.norm.vcf.gz gnomad.genomes.r2.0.1.sites.17.norm.vcf.gz gnomad.genomes.r2.0.1.sites.18.norm.vcf.gz gnomad.genomes.r2.0.1.sites.19.norm.vcf.gz gnomad.genomes.r2.0.1.sites.20.norm.vcf.gz gnomad.genomes.r2.0.1.sites.21.norm.vcf.gz gnomad.genomes.r2.0.1.sites.22.norm.vcf.gz gnomad.genomes.r2.0.1.sites.X.norm.vcf.gz | bgzip -c > gnomad.genomes.r2.0.1.sites.wholeGenome.norm.vcf.gz
+vcf-concat gnomad.genomes.r2.0.2.sites.chr1.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr2.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr3.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr4.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr5.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr6.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr7.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr8.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr9.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr10.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr11.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr12.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr13.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr14.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr15.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr16.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr17.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr18.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr19.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr20.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr21.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chr22.norm.vcf.bgz gnomad.genomes.r2.0.2.sites.chrX.norm.vcf.bgz | .bgzip -c > gnomad.genomes.r2.0.2.sites.chrwholeGenome.norm.vcf.bgz
 
