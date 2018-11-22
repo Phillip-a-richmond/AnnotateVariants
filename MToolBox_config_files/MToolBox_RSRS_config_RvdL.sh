f647b5e5f1adebd85873e1eeb4d215e0b1c988b7
Robin van der Lee <robinvanderlee@gmail.com>
2018-11-22 14:16:53 -0800

    Include MToolBox config files

---
+++ MToolBox_config_files/MToolBox_RSRS_config_RvdL.sh
@@ -0,0 +1,5 @@
+# By default, MToolBox adopts the RSRS (Reconstructed Sapiens Reference Sequence, PMID: 22482806) as mitochondrial reference genome and hg19 as nuclear reference genome
+input_type=fastq
+ref=RSRS
+vcf_name=RSRS
+output_name=. # output dir within current working dir
