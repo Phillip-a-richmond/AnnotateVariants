bed947c9fb0272e150a58406d398bc7146af9d0d
AWS ParallelCluster user <ubuntu@ip-10-255-8-206.ca-central-1.compute.internal>
2022-04-11 21:49:44 +0000

    adding functional excord

---
+++ Cloud/PullTrioRunExcord_complete.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+## CPU Usage
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=32
+## Output and Stderr
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.error
+# #SBATCH --array=201-300%50
+
+##########
+# Set up #
+##########
+
+
+# open up scratch
+sudo chmod ugo=rwx -R /scratch/
+sudo chmod ugo=rwx -R /shared/SVOutput/
+
+# Set a log file
+# Timing from here to end of script
+FullRunStart=`date +%s`
+currentTime=`date`
+echo "Timestamp. Starting analysis: $currentTime"
+echo "In seconds: $FullRunStart"
+
+##########
+# Part 1 #
+##########
+
+# Here we will arrange which trio sample we are working on
+
+# Running for this trio stored in a ped file. All the ped files are stored in that directory, which we will loop through in the next script
+# Setting this for dev before running the array
+SLURM_ARRAY_TASK_ID=0
+
+# Found this online. real useful
+# https://stackoverflow.com/questions/21668471/bash-script-create-array-of-all-files-in-a-directory
+shopt -s nullglob
+
+# Pulling from our list of ped files here
+pedfiles=(/shared/AnnotateVariants/Cloud/1kG_Data/*ped)
+pedfile=${pedfiles[$SLURM_ARRAY_TASK_ID]}
+echo $pedfile
+
+# Use awk to get the sample ids. I know that the father is line 2 (NR==2) and is the first column ($1)
+# mother is line 3, child is line 4
+fatherid=$( awk 'NR==2 {print $1}' $pedfile)
+motherid=$( awk 'NR==3 {print $1}' $pedfile)
+childid=$( awk 'NR==4 {print $1}' $pedfile)
+
+# Check the ids
+echo $fatherid
+echo $motherid
+echo $childid
+
+### Set working space
+# move this back to scratch in production
+Working_Dir=/shared/$childid
+mkdir -p $Working_Dir
+cd $Working_Dir
+
+##########
+# Part 2 #
+##########
+
+# Download the data for father, mother, child from s3, and rename it
+Start=`date +%s`
+currentTime=`date`
+echo "Timestamp. Step2-Start: Downloading data, $currentTime"
+echo "In seconds: $Start"
+
+
+# download father data
+aws s3 cp --dryrun --recursive --no-sign-request s3://1000genomes/data/$fatherid/alignment/ .
+aws s3 cp --recursive --no-sign-request s3://1000genomes/data/$fatherid/alignment/ .
+# rename it
+mv $fatherid*cram $fatherid.cram
+mv $fatherid*cram.crai $fatherid.cram.crai
+
+# download mother data
+aws s3 cp --dryrun --recursive --no-sign-request s3://1000genomes/data/$motherid/alignment/ .
+aws s3 cp --recursive --no-sign-request s3://1000genomes/data/$motherid/alignment/ .
+# rename it
+mv $motherid*cram $motherid.cram
+mv $motherid*cram.crai $motherid.cram.crai
+
+# download child data
+aws s3 cp --recursive --no-sign-request --dryrun  s3://1000genomes/1000G_2504_high_coverage/additional_698_related/data/ ./ --exclude "*" --include "*$childid*"
+aws s3 cp --recursive --no-sign-request  s3://1000genomes/1000G_2504_high_coverage/additional_698_related/data/ ./ --exclude "*" --include "*$childid*"
+mv ./ERR*/$childid*cram $childid.cram
+mv ./ERR*/$childid*cram.crai $childid.cram.crai
+
+
+# Where we pull the reads to, this is on the scratch space (onboard NVMe) 
+CRAM_Dir=$Working_Dir
+
+### For final output
+Final_Dir=/shared/SVOutput/
+mkdir -p $Final_Dir
+
+#### GRCh38 #### 
+echo "GRCh38 genome"
+Genome=GRCh38
+Seq_Type=WGS
+Fasta_Dir=/shared/AnnotateVariants/Cloud/Genomes/
+Fasta_File=GRCh38_full_analysis_set_plus_decoy_hla.fa
+
+End=`date +%s`
+runtime=$((End-Start))
+currentTime=`date`
+echo "Timestamp. Step2-End: Downloading data, $currentTime"
+echo "In seconds: $End"
+echo "Step2 Runtime: $runtime"
+
+
+##########
+# Part 3 #
+##########
+
+Start=`date +%s`
+currentTime=`date`
+echo "Timestamp. Step3-Start: Excord,  $currentTime"
+echo "In seconds: $Start"
+
+# Get discordant reads in bed file
+
+## Get tool
+wget -c -q -O excord https://github.com/brentp/excord/releases/download/v0.2.2/excord_linux64
+chmod +x ./excord
+
+## Activate miniconda environment
+source /shared/AnnotateVariants/Cloud/miniconda3/etc/profile.d/conda.sh
+conda activate /shared/AnnotateVariants/Cloud/miniconda3/envs/Mamba/envs/SeqTools
+
+Sample_ID=$childid
+Sample_CRAM=$Sample_ID.cram
+
+samtools view -b -u -T $Fasta_Dir/$Fasta_File $CRAM_Dir/$Sample_ID.cram | \
+        $CRAM_Dir/excord \
+        --discordantdistance 500 \
+        --fasta $Fasta_Dir/$Fasta_File \
+        /dev/stdin \
+        | LC_ALL=C sort --buffer-size 2G -k1,1 -k2,2n -k3,3n \
+        | bgzip -c > $CRAM_Dir/$Sample_ID.bed.gz
+
+cp $CRAM_Dir/$Sample_ID.bed.gz $Final_Dir
+
+End=`date +%s`
+runtime=$((End-Start))
+currentTime=`date`
+echo "Timestamp. Step3-End: Running excord,  $currentTime"
+echo "In seconds: $End"
+echo "Step3 Runtime: $runtime"
+
+
+FullRunEnd=$End
+currentTime=`date`
+FullRuntime=$((FullRunEnd-FullRunStart))
+echo "Timestamp. Final. $currentTime"
+echo "In seconds: $FullRunEnd"
+echo "Full Runtime: $FullRuntime"
+
+
+exit
+
